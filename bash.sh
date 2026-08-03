0d82a31767cc1a2e0870f561111a233a101fb362
Seriki Walter Yakub <webapp4@outlook.com>
2026-08-03 20:26:52 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,6 @@
 node --input-type=module --eval "import { sep } from 'node:path'; console.log(sep);"
 
 echo "import { sep } from 'node:path'; console.log(sep);" | node --input-type=module
-
+git clone https://github.com/pjreddie/darknet
+cd darknet
+make
